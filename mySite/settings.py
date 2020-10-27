fdd823583bb4868bff868dce0212bd48add60fcb
unknown <samridhigarg2000@gmail.com>
2020-10-27 12:51:33 +0530

    modified

--- mySite/settings.py
+++ mySite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6=*@oz+ce@l4xad85&6ejejh!_5n*-tjzpq+)u8xdbfn5wxv2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['webandblogssss.herokuapp.com/','127.0.0.1']
 
